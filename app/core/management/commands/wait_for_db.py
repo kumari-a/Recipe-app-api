a856b85d80530be9ec9e3e8fbf88950b8f6a9629
k10ab <k10abhi@gmail.com>
2022-11-13 22:33:32 +0530

    added db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -12,8 +12,7 @@ from django.core.management.base import BaseCommand
 class Command(BaseCommand):
     """Django command to wait for database."""
 
-    def handle(self, *args, **options):
-      
+    def handle(self, *args, **options):      
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
@@ -26,3 +25,4 @@ class Command(BaseCommand):
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
+        
