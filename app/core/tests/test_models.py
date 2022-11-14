472d7537f881bd28649a209f2d2927d5d056d945
k10ab <k10abhi@gmail.com>
2022-11-14 23:12:42 +0530

    creater user model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,7 @@ Test for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
@@ -41,3 +42,10 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
